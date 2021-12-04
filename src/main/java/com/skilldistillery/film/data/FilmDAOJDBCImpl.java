85c8e373f31c8fc9fb4c1d08760f903915c3f5ff
Max McGregor <maxmcgregor1@gmail.com>
2021-12-03 18:50:11 -0600

    project setup completed

---
+++ src/main/java/com/skilldistillery/film/data/FilmDAOJDBCImpl.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.film.data;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FilmDAOJDBCImpl implements FilmDAO {
+
+	//TODO: Copy code from DatabaseAccessorObject
+}
